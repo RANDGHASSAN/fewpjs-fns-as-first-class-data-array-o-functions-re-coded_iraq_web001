da50c822b0c483c18697c2c14a52c2720254b1e3
rand  ghassan <randghassan93@gmail.com>
2020-07-11 22:39:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function generalized() {
+  console.log("Wake dog");
+  console.log("lleashdog");
+   console.log("walkToPark");
+    console.log("throwFrisbee");
+     console.log("walkHomeg");
+       console.log("unleashDog");
+}
